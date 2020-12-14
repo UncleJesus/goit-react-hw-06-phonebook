d8a141be8b64412171d7889191074b7ba319bf9b
Омельянов Дима <domelyanov@hotmail.com>
2020-12-14 23:26:39 +0200

    c

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,25 +1,19 @@
-import React from 'react';
-
-import Contacts from '../Contacts/Contacts';
-import ContactForm from '../ContactForm/ContactForm';
-import Filter from '../FilterContacts/FilterContacts';
-
-import Style from './App.module.css'
-
+import React from "react";
+import Contacts from "../Contacts/Contacts";
+import ContactForm from "../ContactForm/ContactForm";
+import Filter from "../FilterContacts/FilterContacts";
+// import Style from "./App.module.css";
 
 const App = function () {
-    
-    return (
-      <div className={Style.Container}>
-        
-        <h1 className={Style.fontColor}>Phonebook</h1>
-        <ContactForm />
-        <h2>Find contacts by name</h2>
-        <Filter />
-        <Contacts />
-      </div>
-    )
-  
-}
+  return (
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Find contacts by name</h2>
+      <Filter />
+      <Contacts />
+    </div>
+  );
+};
 
 export default App;
